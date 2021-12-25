b1779bba3f749031a3478eb654b6f34d9ffcca53
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-26 02:40:52 +0300

    Add solution

---
+++ HackerRank/Practice/Tutorials/30-Days-of-Code/C#/Day-3-Intro-to-Conditional-Statements/Solution/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+            if ((N % 2) == 1)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                if ((N >= 2) && (N <= 5))
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if ((N >= 6) && (N <= 20))
+                {
+                    Console.WriteLine("Weird");
+                }
+                else
+                {
+                    Console.WriteLine("Not Weird");
+                }
+            }
+        }
+    }
+}
