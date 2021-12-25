65640f8e070479f0684273c9631a3dc3962f6227
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-26 02:26:26 +0300

    Add solution

---
+++ HackerRank/Practice/Tutorials/30-Days-of-Code/C#/Day-1-Data-Types/Solution/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            // Declare second integer, double, and String variables.
+            int i2;
+            double d2;
+            string s2;
+
+            // Read and save an integer, double, and String to your variables.
+            i2 = int.Parse(Console.ReadLine());
+            d2 = double.Parse(Console.ReadLine());
+            s2 = Console.ReadLine();
+
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine("{0}", (i + i2));
+
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine("{0:F1}", (d + d2));
+
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+            Console.WriteLine("{0}", (s + s2));
+        }
+    }
+}
