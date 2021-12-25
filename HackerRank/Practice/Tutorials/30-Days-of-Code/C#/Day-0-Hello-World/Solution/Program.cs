4966e0d3f46991df03ee270163ce25864ab221d2
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-26 02:20:05 +0300

    Add solution

---
+++ HackerRank/Practice/Tutorials/30-Days-of-Code/C#/Day-0-Hello-World/Solution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a variable named 'inputString' to hold our input.
+            String inputString;
+
+            // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+            inputString = Console.ReadLine();
+
+            // Print a string literal saying "Hello, World." to stdout using cout.
+            Console.WriteLine("Hello, World.");
+
+            // TODO: Write a line of code here that prints the contents of input_string to stdout.
+            Console.WriteLine(inputString);
+        }
+    }
+}
