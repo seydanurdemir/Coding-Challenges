f5f10acb9892a2514d7788c7dd160d6a901fc630
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-26 02:37:26 +0300

    Add solution

---
+++ HackerRank/Practice/Tutorials/30-Days-of-Code/C#/Day-2-Operators/Solution/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Solution
+{
+    class Result
+    {
+
+        /*
+        * Complete the 'solve' function below.
+        *
+        * The function accepts following parameters:
+        *  1. DOUBLE meal_cost
+        *  2. INTEGER tip_percent
+        *  3. INTEGER tax_percent
+        */
+
+        public static void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            Console.WriteLine(Math.Round(((meal_cost) + ((meal_cost * tip_percent) / 100) + ((meal_cost * tax_percent) / 100))));
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meal_cost = Convert.ToDouble(Console.ReadLine().Trim());
+
+            int tip_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int tax_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Result.solve(meal_cost, tip_percent, tax_percent);
+        }
+    }
+}
