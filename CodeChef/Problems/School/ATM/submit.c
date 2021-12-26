3c34d9fa140cf39b1c14059fff822c2a3a84c119
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-26 05:19:23 +0300

    Add solution

---
+++ CodeChef/Problems/School/ATM/submit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // your code goes here
+    int withdraw = 0;
+    double balance = 0;
+
+    scanf("%d", &withdraw);
+    scanf("%lf", &balance);
+
+    if ((withdraw % 5) == 0)
+    {
+        if (((double)withdraw + 0.5) < balance)
+        {
+            printf("300f", (balance - ((double)withdraw + 0.5)));
+        }
+        else
+        {
+            printf("%.2f", balance);
+        }
+    }
+    else
+    {
+        printf("%.2f", balance);
+    }
+
+    return 0;
+}
