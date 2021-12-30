3f3004ae265a249871ae348f88b37df03011c9ea
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-30 22:20:20 +0300

    Add solution

---
+++ Coderbyte/Challenges/Medium/DistinctList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DistinctList
+{
+    class Program
+    {
+        static int DistinctList(int[] arr)
+        {
+            int duplicates = 0;
+            int[] duplicated = new int[arr.Length];
+
+            return duplicates;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
