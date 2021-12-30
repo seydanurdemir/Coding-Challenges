9e99627142e1f68a602f995576bb38e6b0cf2283
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-30 22:21:32 +0300

    Add solution

---
+++ Coderbyte/Challenges/Medium/DistinctList.cs
@@ -0,0 +1,22 @@
+/**
+ * Difficulty : Medium
+ * Language   : C# (CSharp) (C Sharp)
+ * My Score   : 10 out of 10
+ */
+
+/**
+ * Distinct List
+ * 
+ * Have the function DistinctList(arr) take the array of numbers
+ * stored in arr and determine the total number of duplicate entries. For 
+ * example if the input is [1, 2, 2, 2, 3] then your program should output 2
+ * because there are two duplicates of one of the elements.
+ * 
+ * Examples
+ * 
+ * Input: [0,-2,-2,5,5,5]
+ * Output: 3
+ * 
+ * Input: [100,2,101,4]
+ * Output: 0
+ */
