35f1859cafe4bbd57930b62f46e31c1a311576b4
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-01-03 20:01:12 +0300

    Add solution

---
+++ Coensio/Assessments/MaxElement/Program.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * Company  : Kron
+ * Language : C#
+ * Level    : New Graduate / Junior
+ */
+
+using System;
+
+namespace MaxElement
+{
+    class Program
+    {
+        public static int MaxElement(int[] arr)
+        {
+            int max = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
