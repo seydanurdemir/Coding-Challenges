fa0283462fc5149266b2c3d4664bdf71fb291e2d
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-02-13 20:07:36 +0300

    Add solution

---
+++ Coensio/Assessments/FindMissing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FindMissing
+{
+    class Program
+    {
+        public static int findMissing(int[] nums)
+        {
+            int miss = 0;
+
+            Array.Sort<int>(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (((i + 1) != nums.Length) && ((nums[i] + 2) != (nums[i + 1])))
+                {
+                    miss = nums[i] + 2;
+                    break;
+                }
+            }
+
+            return miss;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 9, 7, 3 };
+
+            Console.WriteLine(findMissing(nums));
+        }
+    }
+}
